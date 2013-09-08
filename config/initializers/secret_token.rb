dc2d8bef16128662cc276424d8138e6aea983420
shyam <shyammohankanojia@gmail.com>
2013-09-09 04:29:02 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PdfjsDemo::Application.config.secret_key_base = '8efb74977c91375a00d8d094dc6c3866b5cad13bb62113e61dbb518f822296115eebdd22f17d921b5d9089f3b0ef87424137be43d88b803b3afada1c4559c734'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PdfjsDemo::Application.config.session_store :cookie_store, key: '_pdfjs_demo_session'
